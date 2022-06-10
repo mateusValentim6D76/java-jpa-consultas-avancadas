6cf4ce36e13a31242f821d6d29537da5727eb410
mateusValentim6D76 <mateus.valentim19@hotmail.com>
2022-06-10 14:18:09 -0300

    Adicionando mappedBy na anotação para o relacionamento bidirecional

--- src/main/java/br/com/alura/loja/modelo/Pedido.java
+++ src/main/java/br/com/alura/loja/modelo/Pedido.java
@@ -4,11 +4,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Table(name = "pedidos")
@@ -22,6 +25,9 @@ public class Pedido {
 
 	@ManyToOne
 	private Cliente cliente;
+	
+	@OneToMany(mappedBy = "pedido")
+	private List<ItemPedido> itens;
 
 	public Pedido(Cliente cliente) {
 		this.cliente = cliente;
