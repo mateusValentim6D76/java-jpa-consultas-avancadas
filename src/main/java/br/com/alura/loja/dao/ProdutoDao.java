80317ba603a8182803c2605780e6a387731769c5
mateusValentim6D76 <mateus.valentim19@hotmail.com>
2022-06-15 16:55:48 -0300

    Utilizando critéria API

--- src/main/java/br/com/alura/loja/dao/ProdutoDao.java
+++ src/main/java/br/com/alura/loja/dao/ProdutoDao.java
@@ -2,10 +2,16 @@ package br.com.alura.loja.dao;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import br.com.alura.loja.modelo.Produto;
 
@@ -87,4 +93,25 @@ public class ProdutoDao {
 		return createQuery.getResultList();
 	}
 
+	public List<Produto> buscarPorParametrosComCriteria(String nome, BigDecimal preco, LocalDate dataCadastro){
+		
+		CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+		CriteriaQuery<Produto> query = criteriaBuilder.createQuery(Produto.class);
+		Root<Produto> from = query.from(Produto.class);
+		
+		Predicate filtros = criteriaBuilder.and();
+		if (nome != null && !nome.trim().isEmpty()) {
+			filtros = criteriaBuilder.and(filtros, criteriaBuilder.equal(from.get("nome"), nome));
+		}
+		if (preco != null) {
+			filtros = criteriaBuilder.and(filtros, criteriaBuilder.equal(from.get("preco"), preco));
+		}
+		if (dataCadastro != null) {
+			filtros = criteriaBuilder.and(filtros, criteriaBuilder.equal(from.get("dataCadastro"), dataCadastro));
+		}
+		query.where(filtros);
+		
+		return em.createQuery(query).getResultList();
+	}
+	
 }
